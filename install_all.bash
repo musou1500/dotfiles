1f6c716f6eacc09e448441486ef189c96e99a215
musou1500 <musou1500@gmail.com>
2017-02-23 01:17:07 +0900

    add Linux support

--- install_all.bash
+++ install_all.bash
@@ -1,27 +1,59 @@
 #!/usr/bin/env bash
 
+function confirm {
+  MSG=$1
+  while :
+  do
+    echo -n "${MSG} [Y/n]: "
+    read ans
+    if [ -z "$ans" ]; then
+      ans="y"
+    fi
+    case $ans in
+      [yY]) return true ;;
+      [nN]) return false ;;
+    esac
+  done
+}
+
+function backup_and_create_symlink {
+  src=$1
+  dst=$2
+  if [ -f dst ] then;
+    mv "$dst" "$dst.org"
+    echo "backup to $dst to $dst.org"
+  fi
+
+  ln -s $src $dst
+}
+
 echo "Make symlinks..."
 scriptdir = `dirname $0`
-ln -s "$scriptdir/.bash_profile" "$HOME/.bash_profile"
-ln -s "$scriptdir/.dircolors" "$HOME/.dircolors"
-ln -s "$scriptdir/.tmux.conf" "$HOME/.tmux.conf"
-ln -s "$scriptdir/.gitconfig" "$HOME/.gitconfig"
 
-source "$HOME/.bash_profile"
+case $OSTYE in
+  darwin*)
+    BASH_CONFIG_FILEPATH="$HOME/.bash_profile"
+    ;;
+  *)
+    BASH_CONFIG_FILEPATH="$HOME/.bashrc"
+    ;;
+esac
+
+backup_and_create_symlink "$scriptdir/.bash_profile" "$BASH_CONFIG_FILEPATH"
+backup_and_create_symlink "$scriptdir/.dircolors" "$HOME/.dircolors"
+backup_and_create_symlink "$scriptdir/.tmux.conf" "$HOME/.tmux.conf"
+backup_and_create_symlink "$scriptdir/.gitconfig" "$HOME/.gitconfig"
+
+# load bash configuration to export xdg variables
+source "$BASH_CONFIG_FILEPATH"
 
 if [ ! -d "$XDG_DATA_HOME/bash_it" ]; then
   echo "Install Bash-it"
-  if [ ! -d $XDG_DATA_HOME ]; then
-    mkdir -p $XDG_DATA_HOME
-  fi
   git clone "https://github.com/Bash-it/bash-it.git" "$XDG_DATA_HOME/bash_it"
 fi
 
 if [ ! -d "$XDG_CONFIG_HOME/bash_it" ]; then
   echo "Install Bash-it custom configuration"
-  if [ ! -d $XDG_CONFIG_HOME ]; then
-    mkdir -p $XDG_CONFIG_HOME
-  fi
   git clone "https://github.com/musou1500/bash-config.git" "$XDG_CONFIG_HOME/bash_it"
 fi
 
@@ -34,17 +66,15 @@ fi
 dein_repo_dir = "$XDG_CACHE_HOME/dein/repos"
 if [ ! -d "$dein_repo_dir/github.com/Shougo/dein.vim" ]; then
   echo "Install dein.vim"
-  mkdir -p "$dein_repo_dir/github.com/Shougo"
   git clone "https://github.com/Shougo/dein.vim" "$dein_repo_dir/github.com/Shougo/dein.vim"
 fi
 
 
 if [ ! -d "$XDG_DATA_HOME/nvm" ]; then
+  echo "Install nvm"
   git clone "https://github.com/creationix/nvm.git" "$XDG_DATA_HOME/nvm"
 fi
 
-echo "Done! reload now? [y/n]"
-read reload_now
-if [ $reload_now = "Y" ] || [ $reload_now = "y" ]; then
+if confirm "Done! Reload bash configuration now?" then
   source "$HOME/.bash_profile"
 fi
