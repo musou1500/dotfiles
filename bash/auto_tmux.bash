bafae68384df1fd2ca2391e33b22606aba79ca6a
musou1500 <musou1500@gmail.com>
2020-10-21 19:12:27 +0900

    セッションが存在しない場合，新しいセッションを作るか確認するように

--- bash/auto_tmux.bash
+++ bash/auto_tmux.bash
@@ -2,17 +2,18 @@
 if [[ -z $TMUX && $- == *i* && $TERM_PROGRAM != "vscode" ]]; then
   # get the IDs
   source "$BASH_IT_CUSTOM/gruvbox_256palette.plugin.bash"
+  create_new_session="Create New Session"
   ID="`tmux list-sessions`"
-  if [[ -z "$ID" ]]; then
-    exec tmux new-session
-  else
-    create_new_session="Create New Session"
+  if [[ -n "$ID" ]]; then
     ID=$ID$'\n'$create_new_session
-    ID="`echo "$ID" | fzf | cut -d: -f1`"
-    if [[ "$ID" = "$create_new_session" ]]; then
-      exec tmux new-session
-    elif [[ -n "$ID" ]]; then
-      exec tmux attach-session -t "$ID"
-    fi
+  else
+    ID=$create_new_session
+  fi
+
+  ID="`echo "$ID" | fzf | cut -d: -f1`"
+  if [[ "$ID" = "$create_new_session" ]]; then
+    exec tmux new-session
+  elif [[ -n "$ID" ]]; then
+    exec tmux attach-session -t "$ID"
   fi
 fi
